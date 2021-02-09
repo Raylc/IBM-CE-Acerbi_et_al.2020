4c6343e638e48eb3032a782de2619a146d9ff072
Cheng Liu <raylc1996@outlook.com>
2021-02-09 01:04:31 -0500

    Finished Chapter 2's model replication

---
+++ Unbiased and biased mutation model.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+
+#unbiased transmission model
+unbiased_mutation <- function(N, mu, p_0, t_max, r_max) {
+  # Create the output tibble
+  output <- tibble(generation = rep(1:t_max, r_max), 
+                   p = as.numeric(rep(NA, t_max * r_max)), 
+                   run = as.factor(rep(1:r_max, each = t_max))) 
+  
+  for (r in 1:r_max) {
+    population <- tibble(trait = sample(c("A", "B"), N, replace = TRUE, 
+                                        prob = c(p_0, 1 - p_0)))
+    # Add first generation's p for run r
+    output[output$generation == 1 & output$run == r, ]$p <- 
+      sum(population$trait == "A") / N 
+    for (t in 2:t_max) {
+      # Copy individuals to previous_population tibble
+      previous_population <- population 
+      
+      # Determine 'mutant' individuals
+      mutate <- sample(c(TRUE, FALSE), N, prob = c(mu, 1 - mu), replace = TRUE) 
+      
+      # If there are 'mutants' from A to B
+      if (nrow(population[mutate & previous_population$trait == "A", ]) > 0) { 
+        # Then flip them to B
+        population[mutate & previous_population$trait == "A", ]$trait <- "B" 
+      }
+      
+      # If there are 'mutants' from B to A
+      if (nrow(population[mutate & previous_population$trait == "B", ]) > 0) { 
+        # Then flip them to A
+        population[mutate & previous_population$trait == "B", ]$trait <- "A" 
+      }
+      
+      # Get p and put it into output slot for this generation t and run r
+      output[output$generation == t & output$run == r, ]$p <- 
+        sum(population$trait == "A") / N 
+    }
+  }
+  # Export data from function
+  output 
+}
+
+# Model testing. The key thing is that unbiased mutation applied individually
+# so regardless of initial trait distribution it will converge to 0.5.
+data_model <- unbiased_mutation(N = 100, mu = 0.05, p_0 = 0.5, t_max = 200, r_max = 5)
+plot_multiple_runs(data_model)
+
+
+
+
+
+#biased transmission model
+biased_mutation <- function(N, mu_b, p_0, t_max, r_max) {
+  # Create the output tibble
+  output <- tibble(generation = rep(1:t_max, r_max), 
+                   p = as.numeric(rep(NA, t_max * r_max)), 
+                   run = as.factor(rep(1:r_max, each = t_max)))
+  
+  for (r in 1:r_max) {
+    population <- tibble(trait = sample(c("A", "B"), N, replace = TRUE, 
+                                        prob = c(p_0, 1 - p_0)))
+    # Add first generation's p for run r
+    output[output$generation == 1 & output$run == r, ]$p <- 
+      sum(population$trait == "A") / N 
+    for (t in 2:t_max) {
+      # Copy individuals to previous_population tibble
+      previous_population <- population 
+      
+      # Determine 'mutant' individuals
+      mutate <- sample(c(TRUE, FALSE), N, prob = c(mu_b, 1 - mu_b), replace = TRUE) 
+      
+      # If there are 'mutants' from B to A
+      if (nrow(population[mutate & previous_population$trait == "B", ]) > 0) {
+        # Then flip them to A
+        population[mutate & previous_population$trait == "B", ]$trait <- "A"
+      }
+      # Get p and put it into output slot for this generation t and run r
+      output[output$generation == t & output$run == r, ]$p <- 
+        sum(population$trait == "A") / N 
+    }
+  }
+  # Export data from function
+  output 
+}
+
+
+data_model <- biased_mutation(N = 100, mu_b = 0.05, p_0 = 0, t_max = 200, r_max = 5)
+plot_multiple_runs(data_model)
--- Unbiased oblique transmission model.R
+++ Unbiased oblique transmission model.R
@@ -1,3 +1,139 @@
+library(tidyverse)
+
+##unbiased trasmission model with single run with fixed initial trait frequency
+unbiased_transmission_1 <- function(N, t_max) {
+  population <- tibble(trait = sample(c("A", "B"), N, replace = TRUE))
+  
+  output <- tibble(generation = 1:t_max, p = rep(NA, t_max))
+  
+  output$p[1] <- sum(population$trait == "A") / N
+  
+  for (t in 2:t_max) {
+    # Copy individuals to previous_population tibble
+    previous_population <- population 
+    
+    # Randomly copy from previous generation
+    population <- tibble(trait = sample(previous_population$trait, N, replace = TRUE))
+    
+    # Get p and put it into output slot for this generation t
+    output$p[t] <- sum(population$trait == "A") / N 
+  }
+  # Export data from function
+  output
+  
+
+}
+
+#Save the simulation outputs in to data_model 
+data_model1 <- unbiased_transmission_1(N = 100, t_max = 200)
+
+#Plotting function and its realization
+plot_single_run <- function(data_model1) {
+  ggplot(data = data_model1, aes(y = p, x = generation)) +
+    geom_line() +
+    ylim(c(0, 1)) +
+    theme_bw() +
+    labs(y = "p (proportion of individuals with trait A)")
+}
+
+plot_single_run(data_model1)
+
+
+
+
+
+##unbiased trasmission model with multiple runs with fixed initial trait frequency
+unbiased_transmission_2 <- function(N, t_max, r_max) {
+  output <- tibble(generation = rep(1:t_max, r_max), 
+                   p = as.numeric(rep(NA, t_max * r_max)), 
+                   run = as.factor(rep(1:r_max, each = t_max))) 
+  # For each run
+  for (r in 1:r_max) { 
+    # Create first generation
+    population <- tibble(trait = sample(c("A", "B"), N, replace = TRUE))
+    
+    # Add first generation's p for run r
+    output[output$generation == 1 & output$run == r, ]$p <-
+      sum(population$trait == "A") / N 
+    
+    # For each generation
+    for (t in 2:t_max) {
+      # Copy individuals to previous_population tibble
+      previous_population <- population 
+      
+      # Randomly copy from previous generation
+      population <- tibble(trait = sample(previous_population$trait, N, replace = TRUE))
+      
+      # Get p and put it into output slot for this generation t and run r
+      output[output$generation == t & output$run == r, ]$p <- 
+        sum(population$trait == "A") / N 
+    }
+  }
+  # Export data from function
+  output 
+}
+
+#Save the simulation outputs in to data_model 
+data_model2 <- unbiased_transmission_2(N = 100, t_max = 200, r_max = 5)
+
+#Plotting function and its realization
+plot_multiple_runs <- function(data_model2) {
+  ggplot(data = data_model2, aes(y = p, x = generation)) +
+    geom_line(aes(colour = run)) +
+    stat_summary(fun = mean, geom = "line", size = 1) +
+    ylim(c(0, 1)) +
+    theme_bw() +
+    labs(y = "p (proportion of individuals with trait A)")
+}
+
+plot_multiple_runs(data_model2)
+
+
+##unbiased trasmission model with multiple runs with non-fixed initial trait frequency
+unbiased_transmission_3 <- function(N, p_0, t_max, r_max) {
+  output <- tibble(generation = rep(1:t_max, r_max), 
+                   p = as.numeric(rep(NA, t_max * r_max)), 
+                   run = as.factor(rep(1:r_max, each = t_max)))
+  # For each run
+  for (r in 1:r_max) {
+    # Create first generation
+    population <- tibble(trait = sample(c("A", "B"), N, replace = TRUE, 
+                                        prob = c(p_0, 1 - p_0)))
+    
+    # Add first generation's p for run r
+    output[output$generation == 1 & output$run == r, ]$p <- 
+      sum(population$trait == "A") / N 
+    
+    for (t in 2:t_max) {
+      # Copy individuals to previous_population tibble
+      previous_population <- population 
+      
+      # Randomly copy from previous generation
+      population <- tibble(trait = sample(previous_population$trait, N, replace = TRUE))
+      
+      # Get p and put it into output slot for this generation t and run r
+      output[output$generation == t & output$run == r, ]$p <- 
+        sum(population$trait == "A") / N  
+    }
+  }
+  # Export data from function
+  output 
+}
+
+# This model shares the same plotting function with model 2
+plot_multiple_runs <- function(data_model3) {
+  ggplot(data = data_model3, aes(y = p, x = generation)) +
+    geom_line(aes(colour = run)) +
+    stat_summary(fun = mean, geom = "line", size = 1) +
+    ylim(c(0, 1)) +
+    theme_bw() +
+    labs(y = "p (proportion of individuals with trait A)")
+}
+
+data_model3 <- unbiased_transmission_3(N = 10000, p_0 = 0.2, t_max = 200, r_max = 5)
+plot_multiple_runs(data_model3)
+
+
 #Preparation
 library(tidyverse)
 #Set up population size, which is fixed across generations.
